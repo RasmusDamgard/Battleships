bf7d019ac416eb380163f94e61c110612c9240c2
Olipus <43773734+Olipus@users.noreply.github.com>
2019-02-06 22:02:01 +0100

    Update Battleships.py

--- Battleships.py
+++ Battleships.py
@@ -9,22 +9,96 @@
 # Licence:     We dont need a license, bitch lasagna
 #-------------------------------------------------------------------------------
 import speech_recognition as sr
+import numpy as np
 from pygame import mixer
 
-#Declare and initiate PyGame Mixer.
+###
+### Variables and modules
+###
+
+# Declare and initiate PyGame Mixer.
 mixer.init()
 
-#Declare speech recognizer software.
+# Declare speech recognizer software.
 r = sr.Recognizer()
 
-def Board(size):
-    for columns in range(size[0]):
-        for rows in range(size[1]):
-            print ("# ", end=" ")
-        print(" ")
+###
+### Classes
+###
+
+class Board:
+    def __init__(self, columns, rows, ships):
+        # Create two identical two-dimensional array.
+        self.state = [[0 for cols in range(columns)] for rows in range(rows)]
+        self.layout = [[0 for cols in range(columns)] for rows in range(rows)]
+        # Safe column and row information in this instance.
+        self.columns = columns
+        self.rows = rows
+        self.ships = ships
+        print("was called")
+
+    def draw_state(self):
+        for c in range(self.columns):
+            for r in range(self.rows):
+                print(self.state[r][c], end=" ")
+            print(" ")
+
+    def draw_layout(self):
+        for c in range(self.columns):
+            for r in range(self.rows):
+                print(self.layout[r][c], end=" ")
+            print(" ")
+
+    def add_ship(self, x, y, direction, length):
+        if(direction == "right"):
+            for i in range(length):
+                self.layout[x-1+i][y-1] = self.ships
+        if(direction == "left"):
+            for i in range(length):
+                self.layout[x-1-i][y-1] = self.ships
+        if(direction == "up"):
+            for i in range(length):
+                self.layout[x-1][y-1-i] = self.ships
+        if(direction == "down"):
+            for i in range(length):
+                self.layout[x-1][y-1+i] = self.ships
+        self.ships -= 1
+
+    def fire(self, x, y):
+        target = self.state[x-1][y-1]
+        #Checks if there has already been fired at this square.
+        if(target == 0): #Have not fired before.
+            hit = self.layout[x-1][y-1]
+            #Checks what was hit.
+            if(hit == 0): #Water
+                self.state[x-1][y-1] = 1
+                print("Miss!")
+            else: #A ship
+                self.state[x-1][y-1] = 2
+                print("Hit!")
+        else: #Have already fired here.
+            print("Already fired here")
 
 
-Board([5,10])
+###
+### Code for execution
+###
 
-#Prevent game from closing.
+
+playerBoard = Board(10, 10, 5)
+computerBoard = Board(6, 6, 5)
+
+playerBoard.add_ship(5, 5, "down", 3)
+playerBoard.fire(5, 6)
+playerBoard.fire(1, 1)
+
+# Update gamescreen
+computerBoard.draw_state()
+print("")
+playerBoard.draw_layout()
+print("")
+playerBoard.draw_state()
+print("")
+
+# Prevent game from closing.
 input("Exit Game?")
