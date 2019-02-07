aabdcb27f223b16528b2e5e162a1da51a5714795
Olipus <43773734+Olipus@users.noreply.github.com>
2019-02-07 18:32:56 +0100

    Updated add_ship

--- Battleships.py
+++ Battleships.py
@@ -21,6 +21,8 @@ mixer.init()
 # Declare speech recognizer software.
 r = sr.Recognizer()
 
+alphabet = ["a","b","c","d","e","g","h","i","j","k","l","m","n","o",]
+
 ###
 ### Classes
 ###
@@ -28,39 +30,51 @@ r = sr.Recognizer()
 class Board:
     def __init__(self, columns, rows, ships):
         # Create two identical two-dimensional array.
-        self.visible = [[0 for cols in range(columns)] for rows in range(rows)]
-        self.hidden = [[0 for cols in range(columns)] for rows in range(rows)]
+        self.visible = [[0 for rows in range(rows)] for cols in range(columns)]
+        self.hidden = [[0 for rows in range(rows)] for cols in range(columns)]
         # Safe column and row information in this instance.
         self.columns = columns
         self.rows = rows
         self.ships = ships
 
     def draw_visible(self):
-        for c in range(self.columns):
-            for r in range(self.rows):
-                print(self.visible[r][c], end=" ")
+        for r in range(self.rows):
+            for c in range(self.columns):
+                print(self.visible[c][r], end=" ")
             print(" ")
 
     def draw_hidden(self):
-        for c in range(self.columns):
-            for r in range(self.rows):
-                print(self.hidden[r][c], end=" ")
+        for r in range(self.rows):
+            for c in range(self.columns):
+                print(self.hidden[c][r], end=" ")
             print(" ")
 
-    def add_ship(self, x, y, direction, length):
-        if(direction == "right"):
-            for i in range(length):
-                self.hidden[x-1+i][y-1] = self.ships
-        if(direction == "left"):
-            for i in range(length):
-                self.hidden[x-1-i][y-1] = self.ships
-        if(direction == "up"):
-            for i in range(length):
-                self.hidden[x-1][y-1-i] = self.ships
-        if(direction == "down"):
-            for i in range(length):
-                self.hidden[x-1][y-1+i] = self.ships
-        self.ships -= 1
+    def add_ship(self, x, y, dirX, dirY, size, checkFirst = True):
+        isValid = True
+        if(x + dirX * size > self.columns):
+            print("Out of bounds")
+            return
+        if(y + dirY * size > self.rows):
+            print("Out of bounds")
+            return
+        if(self.ships < 1):
+            print("All ships used")
+            return
+        for i in range(size):
+            if(checkFirst == True):
+                #If there is anything but water, its not valid.
+                if(self.hidden[x-1+i*dirX][y-1+i*dirY] > 0):
+                    isValid = False
+            else:
+                self.hidden[x-1+i*dirX][y-1+i*dirY] = self.ships
+        if(isValid == False):
+            print("Occupied")
+            return
+        if(checkFirst == True):
+            self.add_ship(x, y, dirX, dirY, size, False)
+        else:
+            self.ships -= 1
+            print("You succesfully placed a ship")
 
     def fire(self, x, y):
         target = self.visible[x-1][y-1]
@@ -77,7 +91,6 @@ class Board:
                 self.hidden[x-1][y-1] += 10
                 print("Hit!")
                 self.checkDestroyed(typeHit)
-
         else: #Have already fired here.
             print("Already fired here")
 
@@ -90,19 +103,21 @@ class Board:
                         self.visible[r][c] = 3
             print("Destroyed a ship")
 
-
 ###
 ### Code for execution
 ###
 
-
 playerBoard = Board(10, 10, 5)
-computerBoard = Board(6, 6, 5)
+computerBoard = Board(10, 6, 5)
 
-playerBoard.add_ship(5, 5, "down", 3)
+#Add a ship at [5,5] heading right [1,0] with size 3.
+playerBoard.add_ship(9, 5, 1, 0, 3)
+#This ship doesnt get placed because it overlaps with the other.
+playerBoard.add_ship(3, 3, 0, 1, 4)
 playerBoard.fire(5, 5)
 playerBoard.fire(5, 6)
 playerBoard.fire(5, 7)
+playerBoard.fire(5, 8)
 
 # Update gamescreen
 computerBoard.draw_visible()
@@ -111,6 +126,9 @@ playerBoard.draw_hidden()
 print("")
 playerBoard.draw_visible()
 print("")
+
+print(computerBoard.columns)
+print(computerBoard.rows)
 
 # Prevent game from closing.
 input("Exit Game?")
